d39d0bc6db298e31c11adbda77e009fde6a84470
Vraj Shah <vraj15297@gmail.com>
2021-04-24 19:18:08 -0400

    SOR done

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 from GenerateMatrix import GenerateMatrix
 from Jacobi import Jacobi
 from GaussSeidel import GaussSeidel
+from SOR import SOR
 
 import numpy as np
 
-system = GenerateMatrix(lower_threshold = 0.6, upper_threshold = 0.91)
+system = GenerateMatrix(lower_threshold = 0.6, upper_threshold = 0.8)
 A, b = system.generate_system(1000)
 
 print("Spectral Radius: ", system.s_radius)
@@ -13,10 +14,19 @@ print("Number of times matrix generated: ", system.counter)
 jacobi_obj = Jacobi(A, b, 1000)
 jacobi_obj.solver()
 
+print("Jacobi done!")
+
 gauss_obj = GaussSeidel(A, b, 1000)
 gauss_obj.solver()
 
+print("Gauss done!")
+
+sor_obj = SOR(A, b, omega = 1)
+sor_obj.solver()
+
 print(str(jacobi_obj))
 print(str(gauss_obj))
+print(str(sor_obj))
 
 print(np.allclose(jacobi_obj.sol, gauss_obj.sol, atol = 1e-6))
+print(np.allclose(jacobi_obj.sol, sor_obj.sol, atol = 1e-6))
--- SOR.py
+++ SOR.py
@@ -1,13 +1,48 @@
 import numpy as np
 
+class MyException(Exception):
+    pass
+    
 class SOR:
-    def __init__(self, A, b, it = 1000):
+    def __init__(self, A, b, initial_guess = 0, omega = 0.5, it = 1000):
         self.A = A
         self.b = b
         self.iterations = it
         self.sol = None
         self.count = 0
-    
+        self.omega = omega
+        self.initial_guess = initial_guess
+        
     def solver(self):
-        pass
-    
+        residual_convergence = 1e-8
+        
+        if(self.initial_guess == 0):
+            self.initial_guess = np.zeros_like(self.b)
+        
+        try:
+            if(self.b.shape != self.initial_guess.shape):
+                raise MyException("b and Initital Guess do not have same shape!")
+        except Exception as e:
+            print("Exception:" + str(e))
+            return
+        
+        self.sol = self.initial_guess[:]
+        
+        curr_residual = np.linalg.norm(np.matmul(self.A, self.sol) - self.b)
+        
+        while(curr_residual > residual_convergence):
+            self.count += 1
+            
+            for i in range(self.A.shape[0]):
+                total = 0
+                
+                for j in range(self.A.shape[1]):
+                    if(j != i):
+                        total += self.A[i, j] * self.sol[j]
+                
+                self.sol[i] = (1 - self.omega) * self.sol[i] + (self.omega / self.A[i, i]) * (self.b[i] - total)
+            
+            curr_residual = np.linalg.norm(np.matmul(self.A, self.sol) - self.b)
+        
+    def __str__(self):
+        return "SOR Iterations: " + str(self.count)
